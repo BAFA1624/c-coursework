5b0dcc2fe65de81b1e2d7b52482c618804be3169
Ben Andrews <benafandrews@gmail.com>
2021-03-08 02:50:59 +0000

    Auto Push: Testing git_push

--- build/git_push.c
+++ build/git_push.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+size_t strlcat(char* dest, const char* src, size_t size);
+
 int main(int argc, char* argv[])
 {
     int status;
-    char* err_msg = "\nUnknown error\n";
+    char* my_err_msg = "\nUnknown error\n";
+    char* push_msg = "\nDefault msg\n";
     if (argc > 1) {
 	status = system("git add ../23762.c ../CMakeLists.txt ../final-exercise2020-2021.pdf ../README.md ../Plots 3_b_1.txt 3_b_2.txt git_push.c inv_1.txt inv_2.txt plot_3c.py") / 256;
 	if (status != 0) {
-	    err_msg = "\nCommand \"git add ....\" failed.\n";
+	    my_err_msg = "\nCommand \"git add ....\" failed.\n";
 	    goto error;
 	}
 	// This is a test
@@ -18,9 +21,9 @@ int main(int argc, char* argv[])
 	// 29
 	size_t sz = strlen(argv[1]) + 29;
 	printf("\nsz = %ld\n", sz);
-	char* push_msg = (char*)malloc(sz * sizeof(char));
+	push_msg = (char*)malloc(sz * sizeof(char));
 	if (!push_msg) {
-	    err_msg = "\nFailed mem alloc for push_msg\n";
+	    my_err_msg = "\nFailed mem alloc for push_msg\n";
 	    goto error;
 	}
 	size_t x = strlcat(push_msg, "git commit -m \"Auto Push: ", sz);
@@ -34,21 +37,26 @@ int main(int argc, char* argv[])
 
 	status = system(push_msg) / 256;
 	if (status != 0) {
-	    err_msg = "\nCommand \"git commit ...\" failed.\n";
+	    my_err_msg = "\nCommand \"git commit ...\" failed.\n";
 	    goto error;
 	}
 
 	status = system("git push") / 256;
 	if (status != 0) {
-	    err_msg = "Command \"git push\" failed.\n";
+	    my_err_msg = "Command \"git push\" failed.\n";
 	    goto error;
 	}
 
     } else {
-	err_msg = "\nError likely due too missing cmd line args.\n";
+	my_err_msg = "\nError likely due too missing cmd line args.\n";
 	goto error;
     }
+    goto cleanup;
 error:
-    fprintf(stderr, "\nAn error occurred.\nEnsure that command is of the form './git_push \"<your message here>\"'.\nErrorMsg: %s", err_msg);
+    fprintf(stderr, "\nAn error occurred.\nEnsure that command is of the form './git_push \"<your message here>\"'.\nErrorMsg: %s", my_err_msg);
     exit(-1);
+cleanup:
+    printf("\nGit push successful! Cleaning up...\n");
+    free(push_msg);
+    printf("\nFinished!\n");
 }
