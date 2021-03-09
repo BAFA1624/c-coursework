e1cbd25af9493a371823b7caf2f2a37e30d6fd65
Ben Andrews <benafandrews@gmail.com>
2021-03-09 19:49:18 +0000

    Auto Push: Removed some unused variables

--- 23762.c
+++ 23762.c
@@ -2,11 +2,18 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 long double pi = 3.14159265358979323846;
 long double pi_2 = 1.57079632679489661923;
 long double pi_4 = 0.78539816339744830962;
 long double e = 2.7182818284590452354;
+
+// Declarations for functions lacking them in header files
+
+#define errno_t int
+#define rsize_t size_t
+errno_t memcpy_s(void* restrict dest, rsize_t destsz, const void* restrict src, rsize_t count);
 
 // -----------------------------------------------------
 // Implementing some simple complex number functionality
@@ -17,14 +24,14 @@ typedef struct Complex {
 } Complex;
 
 // Calculate square of modulus for complex numbers.
-long double c_mod(Complex z) { return sqrtl(z.r * z.r + z.i * z.i); }
-long double c_mod_ptr(Complex* z) { return c_mod(*z); }
+long double c_mod(const Complex z) { return sqrtl(z.r * z.r + z.i * z.i); }
+long double c_mod_ptr(const Complex* z) { return c_mod(*z); }
 
 // Calculate argument of complex number
-long double c_arg(Complex z) { return atan2l(z.i, z.r); }
-long double c_arg_ptr(Complex* z) { return atan2l(z->i, z->r); }
+long double c_arg(const Complex z) { return atan2l(z.i, z.r); }
+long double c_arg_ptr(const Complex* z) { return atan2l(z->i, z->r); }
 
-void print_Complex(Complex* z)
+void print_Complex(const Complex* z)
 {
     printf("(%Lf + %Lfi)", z->r, z->i);
 }
@@ -45,14 +52,14 @@ typedef struct Measurement {
 // Functions which facilitate completing the questions
 
 // Generic write error message, then exit execution
-void error_exit(char* err_msg)
+void error_exit(const char* err_msg)
 {
     fprintf(stderr, err_msg);
     exit(-1);
 }
 
 // Implementation of h1 & h2
-Complex h_1(long double time)
+Complex h_1(const long double time)
 {
     // Calculate h1 using Euler's formula.
     Complex result = {
@@ -61,13 +68,13 @@ Complex h_1(long double time)
     };
     return result;
 }
-Complex h_2(long double time)
+Complex h_2(const long double time)
 {
-    // calculate exponent, x, of h2
-    long double x = (time - pi) * (time - pi) / 2;
+    // calculate exponent, theta, of h2
+    long double theta = (time - pi) * (time - pi) / 2;
     // Initialize Complex number with result.
     Complex result = {
-	.r = powl(e, x),
+	.r = powl(e, theta),
 	.i = 0
     };
     return result;
@@ -111,27 +118,31 @@ Complex* arr_pop_Complex(Complex* arr, size_t N, size_t idx)
 }
 
 // Produces range of N long doubles from start -> end exclusive. Simple version of numpy.linspace
-long double* linspace_ld(long double start, long double end, int N)
+long double* linspace_ld(const long double start, const long double end, const size_t N)
 {
     // Allocate memory for result, calculate increment size for N values in given range
     long double* arr = (long double*)malloc(N * sizeof(long double));
     if (!arr) {
 	error_exit("\nmalloc failed in linspace_ld\n");
     }
-    long double increment = (double)(end - start) / N;
 
-    // Add values to arr, incrementing start every time
-    int i;
+    // Calculate increment for N evenly spaced values from start -> end
+    // 'val' keeps track of current value
+    long double increment = (double)(end - start) / N;
+    long double val = start;
+
+    // Add values to arr, incrementing val every time
+    size_t i;
     for (i = 0; i < N; i++) {
-	arr[i] = start;
-	start += increment;
+	arr[i] = val;
+	val += increment;
     }
 
     return arr;
 }
 
 // Produces range of N complex values for given function func for provided times
-Complex* linspace_Complex(Complex (*func)(long double), long double* times, size_t N)
+Complex* linspace_Complex(Complex (*func)(long double), const long double* times, const size_t N)
 {
     // Allocate memory for result, error if fails
     Complex* arr = (Complex*)malloc(N * sizeof(Complex));
@@ -139,6 +150,7 @@ Complex* linspace_Complex(Complex (*func)(long double), long double* times, size
 	error_exit("\nmalloc failed in linspace_Complex\n");
     }
 
+    // Generate a value in arr for every provided value in times
     size_t i;
     for (i = 0; i < N; ++i) {
 	arr[i] = func(times[i]);
@@ -147,23 +159,28 @@ Complex* linspace_Complex(Complex (*func)(long double), long double* times, size
 }
 
 // Discrete Fourier Transform taking array of samples
-Complex* DFT_samples(Complex* samples, long double* times, int N)
+Complex* DFT_samples(const Complex* samples, const size_t N)
 {
+    // Alloc memory for resulting array & declare vars
     Complex* arr = (Complex*)malloc(N * sizeof(Complex));
     if (!arr) {
 	error_exit("\nmalloc failed in DFT_samples\n");
     }
 
+    // H_n & h_k keep track of which values are currently in use
+    // theta stores value of exponent h_k(t_k).exp(-2.pi.n.k/N) for use in Euler's formula
     Complex H_n, h_k;
     long double theta;
-    int n, k;
+    size_t n, k;
 
+    // For every values H_n, sum contributions of all h_k then add to resulting array
     for (n = 0; n < N; n++) {
+	// initialize H_n
 	H_n.r = 0.;
 	H_n.i = 0.;
 
 	for (k = 0; k < N; k++) {
-	    // Calculate values
+	    // Calculate exponent of h_k(t_k).exp(-2.pi.n.k/N)
 	    theta = 2. * pi * n * k / N;
 	    h_k = samples[k];
 
@@ -180,7 +197,7 @@ Complex* DFT_samples(Complex* samples, long double* times, int N)
 }
 
 // Discrete Fourier Transform taking sample function
-Complex* DFT_function(Complex (*func)(long double), long double* times, int N)
+Complex* DFT_function(Complex (*func)(long double), const long double* times, const int N)
 {
     // Takes:
     // 	- func: Ptr to function to perform DFT on.
@@ -192,7 +209,7 @@ Complex* DFT_function(Complex (*func)(long double), long double* times, int N)
     //  - Returns result
 
     Complex* samples = linspace_Complex(func, times, N);
-    Complex* arr = DFT_samples(samples, times, N);
+    Complex* arr = DFT_samples(samples, N);
 
     return arr;
 }
@@ -204,16 +221,21 @@ void pass() { }
 // Calculates IFT of N provided samples.
 Complex* IFT(Complex* samples, size_t N, size_t skip_n)
 {
+    // Alloc memory for resulting array & declare vars
     Complex* arr = (Complex*)malloc(N * sizeof(Complex));
     if (!arr) {
 	error_exit("\nmalloc failed in IFT\n");
     }
 
+    // H_n & h_k keep track of which values are currently in use
+    // theta stores value of exponent h_k(t_k).exp(-2.pi.n.k/N) for use in Euler's formula
     Complex H_n, h_k;
     long double theta;
     size_t n, k;
 
+    // For every values H_n, sum contributions of all h_k then add to resulting array
     for (k = 0; k < N; k++) {
+	// initialize h_k
 	h_k.r = 0.;
 	h_k.i = 0.;
 
@@ -221,7 +243,7 @@ Complex* IFT(Complex* samples, size_t N, size_t skip_n)
 	    if (n == skip_n) {
 		pass();
 	    } else {
-		// Calculate values
+		// Calculate exponent of H_n(W_n).exp(-2.pi.n.k/N)
 		theta = 2. * pi * n * k / N;
 		H_n = samples[n];
 
@@ -236,12 +258,37 @@ Complex* IFT(Complex* samples, size_t N, size_t skip_n)
 	h_k.i = (long double)h_k.i / N;
 
 	arr[k] = h_k;
-
-	//print_Complex(&result[k]);
-	//putchar('\n');
     }
 
     return arr;
+}
+
+// Compare function for sorting values in 3.k using qsort from stdlib.h
+// Takes const (void*) cast ptrs to two Complex numbers
+// qsort(void* base, size_t nitems, size_t size, int (*compar)(const vooid*, const void*))
+// int return tells qsort which value is >, ==, or < the other
+// result > 0 --> a > b
+// result < 0 --> b > a
+// result = 0 --> a = b
+int compare_Complex(const void* a, const void* b)
+{
+    // Calculate amplitude of Complex vars
+    const long double mod_fa = c_mod_ptr((const Complex*)a);
+    const long double mod_fb = c_mod_ptr((const Complex*)b);
+
+    // Comparison of two Complex numbers
+    // If nan values detected --> error out; else --> return appropriate value for comparison
+    if (isnan(mod_fa) || isnan(mod_fb)) {
+	    error_exit("\nnan values in compare_Complex.\n");
+    } else if (mod_fa > mod_fb) {
+	    return 1;
+    } else if (mod_fa < mod_fb) {
+	    return -1;
+    } else {
+	    return 0;
+    }
+
+    return 0;
 }
 
 // End of general functions
@@ -284,7 +331,7 @@ Complex** q_3b(long double* times, size_t N)
 	y1 = h_1(time);
 	y2 = h_2(time);
 
-	// Write to respective arrays in case we need them later...
+	// Write to respective arrays to they can be used in the next questions
 	*(results[0] + i) = y1;
 	*(results[1] + i) = y2;
 
@@ -400,32 +447,37 @@ Measurement q_3h(const char* filename, size_t N)
 {
     FILE* fp = fopen(filename, "r");
     if (!fp) {
-	error_exit("\nFailed to open data.txt in q_3h.\n");
+	    error_exit("\nFailed to open data.txt in q_3h.\n");
     }
 
     // Allocating vars to keep track of array size and arrays for data
-    size_t sz;
+    size_t sz = 0;
     long double* times = (long double*)malloc(N * sizeof(long double));
     if (!times) {
-	error_exit("\nmalloc failed for times in q_3h.\n");
+	    error_exit("\nmalloc failed for times in q_3h.\n");
     }
+
     int* n_arr = (int*)malloc(N * sizeof(int));
     if (!times) {
-	error_exit("\nmalloc failed for n_arr in q_3h.\n");
+	    error_exit("\nmalloc failed for n_arr in q_3h.\n");
     }
+
     Complex* z_arr = (Complex*)malloc(N * sizeof(Complex));
     if (!z_arr) {
-	error_exit("\nmalloc failed for z_arr in q_3h.\n");
+	    error_exit("\nmalloc failed for z_arr in q_3h.\n");
     }
+
     long double real = 0.;
     long double imag = 0.;
 
     // Use fscanf to read from file
-    while (fscanf(fp, "%d, %e, %e, %e", n_arr[sz], times[sz], real, imag) != EOF && sz < N) {
-	z_arr[sz].r = real;
-	z_arr[sz].i = imag;
-	sz++;
+    while (fscanf(fp, "%d, %Lf, %Lf, %Lf", &n_arr[sz], &times[sz], &real, &imag) != EOF && sz < N) {
+	    z_arr[sz].r = real;
+	    z_arr[sz].i = imag;
+	    sz++;
     }
+
+    fclose(fp);
 
     // Create result & pack it with data from file read
     Measurement result;
@@ -437,17 +489,35 @@ Measurement q_3h(const char* filename, size_t N)
 }
 
 // Apply DFT to h3
-Complex* q_3j(Complex* data, long double* times, size_t N)
+Complex* q_3j(const Complex* data, const size_t N)
 {
     // Applying DFT to data with DFT_samples
-    Complex* result = DFT_samples(data, times, N);
+    Complex* result = DFT_samples(data, N);
     return result;
 }
 
+
+
 // Applies IFT to 4 terms of H3 with largest amplitude
-Complex* q_3k(Complex* samples, size_t N)
+/*Complex* q_3k(const Complex* samples, const size_t N)
 {
-}
+    // Allocating memory for indexes of terms with the four largest amplitudes
+    size_t* idx = (size_t*)malloc(4 * sizeof(size_t));
+    size_t i, max_idx = 0;
+
+    // Copying samples to array for sorting
+    Complex* sorted_samples = (Complex*)malloc(N * sizeof(Complex));
+    errno_t status = memcpy_s((void*)sorted_samples, N * sizeof(Complex), samples, N);
+    if (!status) {
+	error_exit("\nmemcpy_s in q_3k returned failing status.\n");
+    }
+
+    // Sorting samples
+    qsort((void*)sorted_samples, N, sizeof(Complex), compare_Complex);
+
+    //
+    
+}*/
 
 // End of question answers
 // -----------------------------------------------------
@@ -457,7 +527,7 @@ int main()
 {
     // Set number of samples & generate time values
     size_t i, N = 100;
-    long double* times = linspace_ld(0, 2 * pi, 100);
+    long double* times = linspace_ld(0, 2 * pi, N);
 
     // Complete Q3.b, then pass the values --> array of array of Complexes
     Complex** h1_and_h2 = q_3b(times, N);
@@ -469,12 +539,20 @@ int main()
 
     q_3g(prime_h1_and_h2, times, N);
 
-    Measurement h3 = q_3h("data.txt", 200);
+    // Data provided uses N=200
+    N = 200;
 
-    Complex* H3 = q_3j(h3.z_arr, h3.times, 200);
+    Measurement h3 = q_3h("data.txt", N);
+
+    Complex* H3 = q_3j(h3.z_arr, N);
+
+    for (i = 0; i < N; ++i) {
+        printf("\nH3_%ld = ", i);
+        print_Complex(&H3[i]);
+        putchar('\n');
+    }
 
     /*
-
     FILE* fp1 = fopen("inv_1.txt", "w");
     FILE* fp2 = fopen("inv_2.txt", "w");
     if (!fp1 || !fp2) {
