4f3a375f7e8c4f5097a520d5179b7f84e1bd68bd
Ben Andrews <benafandrews@gmail.com>
2021-04-08 05:38:10 +0100

    IFT was broken due to spelling mistakes, fixed now.

---
+++ build/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Complex {
+    long double r;
+    long double i;
+} Complex;
+
+int main()
+{
+
+    printf("\nsizeof(char) = %ld\n", sizeof(char));
+}
