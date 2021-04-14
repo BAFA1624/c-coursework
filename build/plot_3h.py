5fc2b31423072030b66d4621f1a3b4f74818d549
Ben Andrews <benafandrews@gmail.com>
2021-04-14 15:41:57 +0100

    Adding new plots

--- build/plot_3h.py
+++ build/plot_3h.py
@@ -11,97 +11,56 @@ def mod(x, y):
 
 
 # Specifying formatting for axis
-axis_label_font = {'fontname': 'serif',
-                   'size': 18}
-# Set-up empty dicts for data to be read into
+axis_label_font = {'fontname': 'serif','size': 18}
 axes_tick_font = {'fontname': 'serif', 'size': 12}
+legend_font = {'fontsize': 16}
 
 # Open files, write data to respective key, value pair
-h1_prime, h2_prime = {'time': [], 'real': [], 'imag': []}, {
+h2, h2_prime = {'time': [], 'real': [], 'imag': []}, {
     'time': [], 'real': [], 'imag': []}
 # Open files, write data to respective key, value pair
-with open("inv_1.txt", 'r') as file:
-    reader = csv.DictReader(file)
-    for line in reader:
-        for key in line.keys():
-            h1_prime[key].append(np.float64(line[key]))
 with open("inv_2.txt", 'r') as file:
     reader = csv.DictReader(file)
     for line in reader:
         for key in line.keys():
             h2_prime[key].append(np.float64(line[key]))
+with open("h2.txt", 'r') as file:
+    reader = csv.DictReader(file)
+    for line in reader:
+        for key in line.keys():
+            h2[key].append(np.float64(line[key]))
 
-
-# Find y axis min/max for h1 & h2
-h1_r_min, h1_i_min = min(h1_prime['real']), min(h1_prime['imag'])
-h2_r_min, h2_i_min = min(h2_prime['real']), min(h2_prime['imag'])
-
-h1_min, h1_max = min(mod(h1_prime['real'], h1_prime['imag'])), max(
-    mod(h1_prime['real'], h1_prime['imag']))
-
-h2_min, h2_max = min(mod(h2_prime['real'], h2_prime['imag'])), max(
-    mod(h2_prime['real'], h2_prime['imag']))
-
-h1_r_max, h1_i_max = max(h1_prime['real']), max(h1_prime['imag'])
-h2_r_max, h2_i_max = max(h2_prime['real']), max(h2_prime['imag'])
-
-# Set p1/p2_ymin/ymax as respective value
-p1_ymin = 0
-p1_ymax = 0
-p2_ymin = 0
-p2_ymax = 0
-
-if h1_r_min < h1_i_min:
-    p1_ymin = h1_r_min
-else:
-    p1_ymin = h1_i_min
-if h1_r_max > h1_i_max:
-    p1_ymax = h1_r_max
-else:
-    p1_ymax = h1_i_max
-if h2_r_min < h2_i_min:
-    p2_ymin = h2_r_min
-else:
-    p2_ymin = h2_i_min
-if h2_r_max > h2_i_max:
-    p2_ymax = h2_r_max
-else:
-    p2_ymax = h2_i_max
-
-# Plot real & imag part of h1
+#
+# Plot h2
 plt.figure(figsize=(10, 10))
-plt.plot(h1_prime['time'], h1_prime['real'], 'r-',
-         linewidth=1.2, label='real component')
-plt.plot(h1_prime['time'], h1_prime['imag'], 'b-',
-         linewidth=1.2, label='Imaginary component')
-# plt.plot(h1_prime['time'], h1_prime['imag'], 'b-',
-#         linewidth=1.2, label='imaginary component')
+plt.plot(h2['time'], h2['real'], 'r-', 
+        linewidth=1.2, label=r'h$_2$')
+plt.plot(h2_prime['time'], h2_prime['real'], 'b-', 
+        linewidth=1.2, label=r'h$_2$`')
 plt.xlabel('Time / s', **axis_label_font)
-plt.ylabel(r'h$_{1}$`(t)', **axis_label_font)
+plt.ylabel(r'h$_{2}$(t)', **axis_label_font)
 plt.xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi],
-           ['0', r'$\frac{\pi}{2}$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'],
+           ['0', r'$\pi_2$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'],
            **axes_tick_font)
 plt.yticks(**axes_tick_font)
 plt.tick_params(direction='in')
 plt.xlim(0, 2*np.pi)
-#plt.ylim(h1_min, h1_max)
-plt.legend()
+plt.ylim(-20, 140)
+plt.legend(**legend_font)
 plt.show()
 
-# Plot real & imag part of h1
 plt.figure(figsize=(10, 10))
-plt.plot(h2_prime['time'], h2_prime['real'], 'r-',
-         linewidth=1.2, label='real component')
-plt.plot(h2_prime['time'], h2_prime['imag'], 'b-',
-         linewidth=1.2, label='imaginary component')
+plt.plot(h2['time'], h2['imag'], 'r-', 
+        linewidth=1.2, label=r'h$_2$')
+plt.plot(h2_prime['time'], h2_prime['imag'], 'b-', 
+        linewidth=1.2, label=r'h$_2$`')
 plt.xlabel('Time / s', **axis_label_font)
-plt.ylabel(r'h$_{2}$`(t)', **axis_label_font)
+plt.ylabel(r'h$_{2}$(t)', **axis_label_font)
 plt.xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi],
-           ['0', r'$\frac{\pi}{2}$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'],
+           ['0', r'$\pi_2$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'],
            **axes_tick_font)
 plt.yticks(**axes_tick_font)
 plt.tick_params(direction='in')
 plt.xlim(0, 2*np.pi)
-# plt.ylim()
-plt.legend()
+plt.legend(**legend_font)
 plt.show()
--- build/plot_3m.py
+++ build/plot_3m.py
@@ -11,9 +11,9 @@ def mod(x, y):
 
 
 # Specifying formatting for axis
-axis_label_font = {'fontname': 'serif',
-                   'size': 18}
-axes_tick_font = {'fontname': 'serif', 'size': 12}
+axis_label_font = {'fontname': 'serif','size': 18}
+axes_tick_font = {'fontname': 'serif', 'fontsize': 12}
+legend_font = {'fontsize': 16}
 
 # Set-up empty dicts for data to be read into
 h3, i_h3 = {'n': [], 'time': [], 'real': [], 'imag': []}, {
@@ -33,65 +33,38 @@ with open("inv_3.txt", 'r') as file:
         for key in line.keys():
             i_h3[key].append(np.float64(line[key]))
 
-# Find y axis min/max for h1 & h2
-h3_min, h3_max = min(mod(h3['real'], h3['imag'])), max(
-    mod(h3['real'], h3['imag']))
-ih3_min, ih3_max = min(mod(i_h3['real'], i_h3['imag'])), max(
-    mod(i_h3['real'], i_h3['imag']))
-
-h3_r_max, h3_i_max = max(h3['real']), max(h3['imag'])
-ih3_r_max, ih3_i_max = max(i_h3['real']), max(i_h3['imag'])
-
-# Set p1/p2_ymin/ymax as respective value
-p1_ymin = 0
-p1_ymax = 0
-
-if h3_min < ih3_min:
-    p1_ymin = h3_min
-else:
-    p1_ymin = ih3_min
-if h3_max > ih3_max:
-    p1_ymax = h3_max
-else:
-    p1_ymax = ih3_max
-
 
 # Plot real & imag part of h3 & i_h3
 plt.figure(figsize=(10, 10))
-plt.plot(h3['time'], mod(h3['real'], h3['imag']), 'b-', linewidth=1.2,
-         label='Original Signal')
-plt.plot(i_h3['time'], mod(i_h3['real'], i_h3['imag']), 'r--', linewidth=1.2,
-         label='Transformed Version')
+plt.plot(h3['time'], h3['real'], 'r-', linewidth=1.2,
+         label=r'h$_3$')
+plt.plot(i_h3['time'], i_h3['real'], 'b-', linewidth=1.2,
+         label=r'h$_3$`')
 plt.xlabel('Time / s', **axis_label_font)
-plt.ylabel(r'|h$_{3}$(t)|', **axis_label_font)
+plt.ylabel(r'h$_{3}$(t)', **axis_label_font)
 plt.xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi],
            ['0', r'$\pi_2$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'],
            **axes_tick_font)
 plt.yticks(**axes_tick_font)
 plt.tick_params(direction='in')
 plt.xlim(0, 2*np.pi)
-plt.ylim(p1_ymin, p1_ymax)
-plt.legend()
+plt.ylim(-12.5, 12.5)
+plt.legend(**legend_font)
 plt.show()
 
-
 plt.figure(figsize=(10, 10))
-plt.plot(h3['time'], h3['real'], 'b-',
-         linewidth=1.2, label='Original real component')
-plt.plot(i_h3['time'], i_h3['real'], 'g--',
-         linewidth=1.2, label='Transformed real component')
 plt.plot(h3['time'], h3['imag'], 'r-',
-         linewidth=1.2, label='Original imaginary component')
-plt.plot(i_h3['time'], i_h3['imag'], '--', color='orange',
-         linewidth=1.2, label='Transformed imaginary component')
+         linewidth=1.2, label=r'h$_3$')
+plt.plot(i_h3['time'], i_h3['imag'], 'b-',
+         linewidth=1.2, label=r'h$_3$`')
 plt.xlabel('Time / s', **axis_label_font)
-plt.ylabel(r'h$_{3}$`(t)', **axis_label_font)
+plt.ylabel(r'h$_{3}$(t)', **axis_label_font)
 plt.xticks([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi],
            ['0', r'$\pi_2$', r'$\pi$', r'$\frac{3\pi}{2}$', r'$2\pi$'],
            **axes_tick_font)
 plt.yticks(**axes_tick_font)
 plt.tick_params(direction='in')
 plt.xlim(0, 2*np.pi)
-plt.ylim(-11, 11.5)
-plt.legend()
+plt.ylim(-12.5, 12.5)
+plt.legend(**legend_font)
 plt.show()
